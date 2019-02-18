270e083295c29b97197a9bd9a50238d8424ea37c
Matias Ramirez <mnramirez@moneda.cl>
2019-02-18 19:00:52 -0300

    Se agrega funcionalidad al proyecto, ahora muestra los videos, descripción y titulo desde youtube en la página

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-/*$(document).ready(() => alert("JQuery is working"));
+/*$(document).ready(() => alert("JQuery is working"));*/
+
+$(document).ready(() => {
+    const key = 'AIzaSyAmQx5VQU6dDDexBVzRVq0PIMpSyc268SU';
+    const playlistId = 'PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG';
+    const URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
+    var options = {
+        part: 'snippet',
+        key: key,
+        maxResult: 20,
+        playlistId: playlistId
+    };
+
+
+    function loadVids(){
+        $.getJSON(URL, options, function (data) {
+                let id = data.items[0].snippet.resourceId.videoId;
+                mainVideo(id);
+                loopVids(data);           
+                console.log(data);     
+            });
+    }
+
+    loadVids();
+
+    function mainVideo(id){
+        $('.playlist').html(`
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" 
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `);
+    }
+
+    function loopVids(data){
+        
+        $.each(data.items, function (i, item) { 
+            let miniature = item.snippet.thumbnails.high.url; 
+            let title = item.snippet.title;
+            let description = item.snippet.description.substring(0, 100);
+
+            $('.list').append(`
+                <div class="videos">
+                    <img src="${miniature}" alt="" class="thumb">
+                    <div class="details">
+                        <h4>${title}</h4>
+                        <p>${description}</p>
+                    </div>            
+                </div>
+            `); 
+        });
+    }
+
+});
